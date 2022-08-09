4160df1972cea2ed51a3e8b1bc33453152bddca3
Arturito <arturaskisielius91@gmail.com>
2022-08-09 17:50:58 +0300

    css style changes

--- js/fetch.js
+++ js/fetch.js
@@ -1,17 +1,18 @@
 fetch('https://picsum.photos/v2/list')
 .then(res => res.json())
 .then(data => {
-    // console.log(data);
-    // document.getElementById('root').
-    // innerHTML=data[0].author;
     let data1 = '';
     data.map((values) => {
         data1 += `<div class="card">
-                    <h4 class="author">${values.author}</h4>
-                    <span class="height">Size: ${values.height}x${values.width}</span>
                     <img src="${values.download_url}" alt="img">
                 </div>`
     })
     document.getElementById('cards').innerHTML = data1;
 })
-.catch(error => console.log('ERROR'))
+.catch(error => console.log('ERROR'));
+
+{/* <div class="card">
+    <h4 class="author">${values.author}</h4>
+    <p class="dimmensions">Size: ${values.height}x${values.width}</p>
+    <img src="${values.download_url}" alt="img">
+</div> */}
