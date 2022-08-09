b4be41935514ca1412028f70c0aeb2aab29e2550
Arturito <arturaskisielius91@gmail.com>
2022-08-09 17:10:20 +0300

    Leftside photo list

--- js/combined.js
+++ js/combined.js
@@ -2,6 +2,7 @@
 let slider = document.getElementById('slider');
 let output = document.getElementById('value');
 let img = document.getElementById('img');
+let checkboxGray = document.getElementById('check');
 
 // input sliderio 'value=0' yra prilyginamas span tag'e užduotai reikšmei (0);
 output.innerHTML = slider.value;
@@ -14,4 +15,12 @@ slider.addEventListener("mousemove", () => {
     let x = slider.value;
     let blurValue = 'blur(' + (x / 10) + 'px' + ')';
     img.style.filter = blurValue;
+});
+
+// Get checkbox
+
+// Get image
+
+checkboxGray.addEventListener("change", () => {
+    checkboxGray.checked ? img.style.filter = 'grayscale(100%)' : img.style.filter = 'initial';
 });
